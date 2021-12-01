69f28bc4894ac50c9e68ba295d2a31a2826b46c3
tinchorlando <martin._orlando@hotmail.com.ar>
2021-12-01 01:41:57 -0300

    Agregado de Homework 04 incompleta

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0]
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length -1]
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length
 }
 
 
@@ -23,6 +26,9 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for (let i=0;i<array.length;i++){
+    array[i] = array[i] +1
+  }return array
 }
 
 
@@ -30,6 +36,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento)
+  return array
 }
 
 
@@ -38,6 +46,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento)
+  return array
 }
 
 
@@ -47,6 +57,10 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  var frase = palabras [0]
+  for (let i=1;i<palabras.length;i++){
+    frase=frase+' '+palabras[i]
+  }return frase
 }
 
 
@@ -54,6 +68,13 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  var check=false
+  for (let i=0;i<array.length;i++){
+    if (array[i]===elemento){
+      check=true
+    }
+  }return check
+  
 }
 
 
@@ -61,6 +82,11 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  
+  var suma=0 
+  for (let i=0;i<numeros.length;i++){
+    suma=suma+numeros[i]
+  }return suma
 }
 
 
@@ -68,6 +94,10 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var suma=0
+  for (let i=0;i<resultadosTest.length;i++){
+    suma = suma+resultadosTest[i]
+  } return (suma/resultadosTest.length)
 }
 
 
@@ -75,6 +105,14 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var fin=0
+  
+  for (let i=0;i<numeros.length;i++){
+    if (numeros[i]>=fin){
+      fin=numeros [i]
+      
+    }
+  }return fin
 }
 
 
@@ -82,12 +120,28 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var producto=1
+  if (arguments.length===0){
+    return 0
+  } else if (arguments.length===1){
+    return arguments [0]
+  } else 
+      for (let i=0;i<arguments.length;i++){
+        producto=producto*arguments[i]
+      }return producto
+  
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  var contador =0
+  for (let i=0;i<arreglo.length;i++){
+    if (arreglo[i]>18){
+      contador=contador+1
+    }
+  } return contador
 
 }
 
@@ -97,6 +151,10 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+  if (numeroDeDia===7 || numeroDeDia===1){
+    return 'Es fin de semana'
+  } else return 'Es dia Laboral'
+
   
 } 
 
@@ -105,6 +163,14 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  var str 
+  str = n.toString()
+  var salida
+  if(str[0]==9){
+    salida=true
+  }else salida=false
+  return salida
+
   
 }
 
@@ -113,7 +179,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  var chequeo = true
+  for (let i=0;i<arreglo.length;i++){
+    if (arreglo[0]!==arreglo[i]){
+      chequeo=false
+    }
+  }return chequeo
 } 
 
 
@@ -122,6 +193,14 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var brray=[]
+  for (let i=0;i<array.length;i++){
+    if (array[i]=='Enero' || array[i]=='Marzo' || array[i]=='Noviembre'){
+      brray.push(array[i]);
+    }
+  } if ((brray.length) === 3 ){
+    return brray
+  }else return 'No se encontraron los meses pedidos'
 }
 
 
@@ -129,6 +208,12 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var brray =[]
+  for (let i=0;i<array.length;i++){
+    if(array[i]>100){
+      brray.push(array[i])
+    }
+  }return brray
 }
 
 
@@ -140,6 +225,19 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var i=0
+  var array =[]
+  while (i<10){
+    numero=numero+2
+    array.push(numero)
+    i=i+1
+    if(i===numero) break
+    return 'Se interrumpió la ejecución'
+
+    
+    
+
+  } 
 }
 
 
